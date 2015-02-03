1453e0bcd0dd336829b0a06397af6a42bb1ba4ad
Bob Roberts <admin@bobroberts.us>
2015-02-02 19:58:08 -0500

    Issue #31: Changed the comparator so constructors take priority over all other methods

--- src/org/salesforce/apexdoc/ClassModel.java
+++ src/org/salesforce/apexdoc/ClassModel.java
@@ -43,13 +43,21 @@ public class ClassModel extends ApexModel {
     }
 
     public ArrayList<MethodModel> getMethodsSorted() {
-        List<MethodModel> sorted = ( List<MethodModel>)methods.clone();
-        Collections.sort(sorted, new Comparator(){
+        @SuppressWarnings("unchecked")
+		List<MethodModel> sorted = (List<MethodModel>)methods.clone();
+        Collections.sort(sorted, new Comparator<MethodModel>(){
             @Override
-            public int compare(Object o1, Object o2) {
-                String methodName1 = ((MethodModel)o1).getMethodName().toLowerCase();
-                String methodName2 = ((MethodModel)o2).getMethodName().toLowerCase();
-                return (methodName1.compareTo(methodName2));
+            public int compare(MethodModel o1, MethodModel o2) {
+                String methodName1 = o1.getMethodName();
+                String methodName2 = o2.getMethodName();
+                String className = getClassName();
+                
+                if(methodName1.equals(className)){
+                    return Integer.MIN_VALUE;
+                } else if(methodName2.equals(className)){
+                    return Integer.MAX_VALUE;
+                }
+                return (methodName1.toLowerCase().compareTo(methodName2.toLowerCase()));
             }
         });
         return new ArrayList<MethodModel>(sorted);
